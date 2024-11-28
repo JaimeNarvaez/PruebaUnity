46f196cf5ec022784b48ddcb73d305330ed506e4
JaimeNarvaez <jjno1999@gmail.com>
2024-11-27 22:03:22 -0500

    Primer commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform rifleStart;
+    [SerializeField] private Text HpText;
+
+    [SerializeField] private GameObject GameOver;
+    [SerializeField] private GameObject Victory;
+
+    public float health = 0;
+    public float speed = 5f;
+    public float rotationSpeed = 720f;
+
+    void Start()
+    {
+        
+        ChangeHealth(100);
+    }
+
+    public void ChangeHealth(int hp)
+    {
+        health += hp;
+        if (health > 100)
+        {
+            health = 100;
+        }
+        else if (health <= 0)
+        {
+            Lost();
+        }
+        HpText.text = health.ToString();
+    }
+
+    public void Win()
+    {
+        Victory.SetActive(true);
+        Destroy(GetComponent<PlayerLook>());
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Lost()
+    {
+        GameOver.SetActive(true);
+        Destroy(GetComponent<PlayerLook>());
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject buf = Instantiate(bullet);
+            buf.transform.position = rifleStart.position;
+            buf.GetComponent<Bullet>().setDirection(transform.forward);
+            buf.transform.rotation = transform.rotation;
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Collider[] tar = Physics.OverlapSphere(transform.position, 2);
+            foreach (var item in tar)
+            {
+                if (item.tag == "Enemy")
+                {
+                    Destroy(item.gameObject);
+                }
+            }
+        }
+
+        Collider[] targets = Physics.OverlapSphere(transform.position, 3);
+        foreach (var item in targets)
+        {
+            if (item.tag == "Heal")
+            {
+                ChangeHealth(50);
+                Destroy(item.gameObject);
+            }
+            if (item.tag == "Finish")
+            {
+                Win();
+            }
+            if (item.tag == "Enemy")
+            {
+                Lost();
+            }
+        }
+        MovePlayer();
+    }
+    void MovePlayer()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical"); 
+
+       
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized;
+
+        
+        if (movement.magnitude > 0.1f)
+        {
+            
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, 0.1f);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
